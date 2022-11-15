47b2601e13f297d5c9b5e5d3e82cc0be3e028419
ArefAziziii <aziziaref84@gmail.com>
2022-11-15 22:19:26 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,38 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted(){
+    const socket = SockJS('http://localhost:8080/connect')
+    this.stompClient = Stomp.over(socket)
+    
+    // stompClient.connect({},Frame => 
+    // console.log(Frame))
+          this.stompClient.connect(
+        {},
+        frame => {
+        //  this.connected = true;
+          console.log(frame);
+          this.stompClient.subscribe("/topic/messages", tick => {
+          console.log(tick);
+          console.log(JSON.parse(tick.body).content);
+          });
+        },
+        error => {
+          console.log(error);
+         // this.connected = false;
+        }
+      )
+    }
+
   }
-}
 </script>
 
 <style>
